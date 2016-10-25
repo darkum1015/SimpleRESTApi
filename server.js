8173f72acd060cae0139803a7771e9339b74dd17
darkum1015 <darkum09@gmail.com>
2016-10-25 18:31:28 +0530

    added package.js and server.js

---
+++ server.js
@@ -0,0 +1,258 @@
+// =======================
+// get the packages we need ============
+// =======================
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./app/config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
+var Bear   = require('./app/models/bear');
+
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8100; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+app.get('/setup', function(req, res) {
+
+    // create a sample user
+    var darshan = new User({
+        name: 'Darshan',
+        password: 'test123',
+        admin: true
+    });
+
+    // save the sample user
+    darshan.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
+// API ROUTES -------------------
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
+
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
+apiRoutes.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+apiRoutes.route('/bears').post(function(req,res){
+    var bear = new Bear();      // create a new instance of the Bear model
+    bear.name = req.body.name;  // set the bears name (comes from the request)
+
+    // save the bear and check for errors
+    bear.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Bear created!' });
+    });
+
+}).get(function(req, res) {
+    Bear.find(function(err, bears) {
+        if (err)
+            res.send(err);
+
+        res.json(bears);
+    });
+});
+
+
+/*apiRoutes.post('/bears',function(req,res){
+    var bear = new Bear();      // create a new instance of the Bear model
+    bear.name = req.body.name;  // set the bears name (comes from the request)
+
+    // save the bear and check for errors
+    bear.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Bear created!' });
+    });
+
+});
+
+apiRoutes.get('/bears',function(req, res) {
+    Bear.find(function(err, bears) {
+        if (err)
+            res.send(err);
+
+        res.json(bears);
+    });
+});*/
+
+apiRoutes.route('/bears/:bear_id').get(function(req,res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+            res.send(err);
+        res.json(bear);
+    });
+}).put(function(req,res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+            res.send(err);
+
+        bear.name = req.body.name;  // update the bears info
+
+        // save the bear
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear updated!' });
+        });
+    });
+}).delete(function(req, res) {
+    Bear.remove({
+        _id: req.params.bear_id
+    }, function (err, bear) {
+        if (err)
+            res.send(err);
+
+        res.json({message: 'Successfully deleted'});
+    });
+});
+
+/*apiRoutes.get('/bears/:bear_id',function(req,res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+            res.send(err);
+        res.json(bear);
+    });
+});
+
+apiRoutes.put('/bears/:bear_id',function(req,res){
+    Bear.findById(req.params.bear_id, function(err, bear) {
+        if (err)
+            res.send(err);
+
+        bear.name = req.body.name;  // update the bears info
+
+        // save the bear
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear updated!' });
+        });
+    });
+});
+
+apiRoutes.delete('/bears/:bear_id',function(req, res) {
+    Bear.remove({
+        _id: req.params.bear_id
+    }, function (err, bear) {
+        if (err)
+            res.send(err);
+
+        res.json({message: 'Successfully deleted'});
+    });
+});*/
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
