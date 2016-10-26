0bbd5a2f65438d7acce61c634c838cbf26996461
darkum1015 <darkum09@gmail.com>
2016-10-26 11:20:41 +0530

    Updated Bear model and incorporated changes to post method for Bears api

--- app/models/Bear.js
+++ app/models/Bear.js
@@ -2,7 +2,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var BearSchema   = new Schema({
-    name: String
+    name: {type: String},
+    species: {type: String}
 });
 
 module.exports = mongoose.model('Bear', BearSchema);
